08b4563d27988c8f32e575ed3dfc4a1e99827fca
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-10-26 15:53:00 +0530

    init update

--- lib/main.dart
+++ lib/main.dart
@@ -39,29 +39,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// import 'package:flutter/material.dart';
-// import 'splash.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // Segar Dir
-//         colorScheme:
-//             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 150, 235, 153)),
-//         useMaterial3: true,
-//       ),
-//       home: SplashScreen(),
-//     );
-//   }
-// }
